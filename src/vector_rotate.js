2cb26a059fd460a80e4c2d71114969dc9816471f
afghl <airport.0930@gmail.com>
2016-02-11 13:23:08 +0800

    added vector rotate

---
+++ src/vector_rotate.js
@@ -0,0 +1,13 @@
+var VectorRotate = (function() {
+  return {
+    rotate : function(v, object2) {
+      return {
+        object1velocity: [-2, 0],
+        object2velocity: [-22, 0]
+      }
+    }
+  };
+
+})();
+
+module.exports = VectorRotate;
